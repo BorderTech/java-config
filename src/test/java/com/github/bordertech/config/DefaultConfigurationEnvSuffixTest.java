a575dffe56dffadd40229e6efb0484fb1b7254f3
mccabd <dion.mccabe@afsa.gov.au>
2020-10-21 16:36:25 +1000

    Ensure System property is cleared before test

--- src/test/java/com/github/bordertech/config/DefaultConfigurationEnvSuffixTest.java
+++ src/test/java/com/github/bordertech/config/DefaultConfigurationEnvSuffixTest.java
@@ -24,34 +24,33 @@ public class DefaultConfigurationEnvSuffixTest {
 		DefaultConfiguration config = new DefaultConfiguration(
 			"com/github/bordertech/config/DefaultConfigurationEnvSuffixTest.properties");
 
-		System.out.println(config.getProperties().toString());
+		System.clearProperty(ENVIRONMENT_PROPERTY);
 
 		//Test the default value
-		assertPropertyEquals(config, TEST_PROPERTY_KEY, DEFAULT_PROPERTY_VALUE);
+		assertPropertyEquals(config, DEFAULT_PROPERTY_VALUE);
 
 		//Set the environment suffix 1 and reload
 		System.setProperty(ENVIRONMENT_PROPERTY, "suffix1");
 		config.refresh();
 
 		//Test for the suffix 1 is set
-		assertPropertyEquals(config, TEST_PROPERTY_KEY, SUFFIX1_PROPERTY_VALUE);
+		assertPropertyEquals(config, SUFFIX1_PROPERTY_VALUE);
 
 		//Set the environment suffix 2 and reload
 		System.setProperty(ENVIRONMENT_PROPERTY, "suffix2");
 		config.refresh();
 
 		//Test for the suffix 2 is set
-		assertPropertyEquals(config, TEST_PROPERTY_KEY, SUFFIX2_PROPERTY_VALUE);
+		assertPropertyEquals(config, SUFFIX2_PROPERTY_VALUE);
 
 	}
 
 	/**
 	 * Asserts that the configuration contains the given key/value.
 	 *
-	 * @param key the property key
 	 * @param expected the expected property value.
 	 */
-	private void assertPropertyEquals(final DefaultConfiguration config, final String key, final Object expected) {
-		Assert.assertEquals("Incorrect value for " + key, expected, config.get(key));
+	private void assertPropertyEquals(final DefaultConfiguration config, final Object expected) {
+		Assert.assertEquals("Incorrect value for " + TEST_PROPERTY_KEY, expected, config.get(TEST_PROPERTY_KEY));
 	}
 }
